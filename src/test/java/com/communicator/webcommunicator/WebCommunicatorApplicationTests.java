50c07297898608563a57bfc8e91d4c964ddaff15
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-10-02 19:34:36 +0200

    Added WebApp base test class

---
+++ src/test/java/com/communicator/webcommunicator/WebCommunicatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communicator.webcommunicator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCommunicatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
