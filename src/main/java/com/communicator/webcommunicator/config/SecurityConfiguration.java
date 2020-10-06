5b90e7b6ff9e3f023ba80b974c7775f9e3623223
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-10-06 12:36:25 +0200

    Added remember me functionality with persistent remember me keys (H2 database)

--- src/main/java/com/communicator/webcommunicator/config/SecurityConfiguration.java
+++ src/main/java/com/communicator/webcommunicator/config/SecurityConfiguration.java
@@ -1,47 +1,45 @@
 package com.communicator.webcommunicator.config;
 
+import com.communicator.webcommunicator.service.PersistentLoginService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
+    private final PersistentLoginService persistentLoginService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
     }
 
-    // potrzebne permitAll??
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                    .antMatchers("/resources/**").permitAll()
                     .antMatchers("/admin").hasRole("ADMIN")
                     .antMatchers("/user").hasAnyRole("ADMIN", "USER")
-                    .antMatchers("/**").permitAll()
                 .and()
                 .formLogin()
                     .loginPage("/login")
                     .failureUrl("/login-error").permitAll()
+                .and()
+                //TODO Add remember me value to H2, set expiry time
+                .rememberMe().rememberMeParameter("remember-me").tokenRepository(persistentLoginService)
+                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(5))
                 .and()
                 .logout()
                     .logoutUrl("/logout")
